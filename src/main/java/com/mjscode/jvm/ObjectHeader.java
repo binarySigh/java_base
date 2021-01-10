0d26888d16cd53f6a92bbdb0536a7dd7ef0ee1b9
binarySigh <951382408@qq.com>
2021-01-10 20:40:50 +0800

    JVM update

--- src/main/java/com/mjscode/jvm/ObjectHeader.java
+++ src/main/java/com/mjscode/jvm/ObjectHeader.java
@@ -2,16 +2,28 @@ package com.mjscode.jvm;
 
 import org.openjdk.jol.info.ClassLayout;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author binarySigh
  */
 public class ObjectHeader {
     public static void main(String[] args){
+        /*try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
         Object[] arr = new Object[10];
         double[] d = new double[10];
         Object o = new Object();
-        System.out.println(ClassLayout.parseInstance(arr).toPrintable());
+        boolean i = false;
+        Boolean t = false;
+        /*System.out.println(ClassLayout.parseInstance(arr).toPrintable());
         System.out.println(ClassLayout.parseInstance(d).toPrintable());
-        System.out.println(ClassLayout.parseInstance(o).toPrintable());
+        System.out.println(ClassLayout.parseInstance(o).toPrintable());*/
+        System.out.println(ClassLayout.parseInstance(i).toPrintable());
+        System.out.println(ClassLayout.parseInstance(t).toPrintable());
     }
 }
